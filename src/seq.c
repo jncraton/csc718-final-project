6f23041a96af8d050d9c94caf6ff00e6a3bb808a
Jon Craton <jncraton@gmail.com>
2019-11-17 17:27:13 -0500

    Use structs for bodies

--- src/seq.c
+++ src/seq.c
@@ -1,18 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 #define N 100
-#define ITERATIONS 40
-#define LOG_EVERY 1
+#define ITERATIONS 400
+#define LOG_EVERY 20
 
 const double G = 6.674E-11;
-const double STEP_SIZE = 100.0;
+const double STEP_SIZE = 1.0;
 const double earth_radius = 6.357E6;
 
+struct Body {
+  double mass;
+  double x;
+  double y;
+  double z;
+  double dx;
+  double dy;
+  double dz;
+};
 
-double *x, *y, *z, *dx, *dy, *dz, *m;
+struct Body *bodies;
 
-void update_velocity(double *pos, double *vel, double *mass) {
+void update_velocity(struct Body *bodies) {
   //F = GmM/r^2
   //a = Gm/r^2 m is mass of the other, our mass cancels
   //r^2 = distance^2
@@ -22,18 +32,30 @@ void update_velocity(double *pos, double *vel, double *mass) {
   for (int i = 1; i < N; i++) {
     for (int j = 0; j < N; j++) {
       if (i != j) {
-        double sep = pos[j] - pos[i];
-        if (sep > 1.0 || sep < -1.0) {
-          vel[i] += STEP_SIZE*G*mass[j]/(pos[j] - pos[i]);
+        double r2 = (
+          (bodies[i].x-bodies[j].x) * (bodies[i].x-bodies[j].x) +
+          (bodies[i].y-bodies[j].y) * (bodies[i].y-bodies[j].y) +
+          (bodies[i].z-bodies[j].z) * (bodies[i].z-bodies[j].z)
+        );
+
+        if (r2 > 100.0) {
+          bodies[i].dx += ((bodies[j].x - bodies[i].x) / sqrt(r2)) * 
+            STEP_SIZE*G*bodies[j].mass/r2;
+          bodies[i].dy += ((bodies[j].y - bodies[i].y) / sqrt(r2)) * 
+            STEP_SIZE*G*bodies[j].mass/r2;
+          bodies[i].dy += ((bodies[j].z - bodies[i].z) / sqrt(r2)) * 
+            STEP_SIZE*G*bodies[j].mass/r2;
         }
       }
     }
   }
 }
 
-void update_position(double *pos, double *vel) {
+void update_position(struct Body *bodies) {
   for (int i = 1; i < N; i++) {
-    pos[i] += vel[i] * STEP_SIZE;
+    bodies[i].x += bodies[i].dx * STEP_SIZE;
+    bodies[i].y += bodies[i].dy * STEP_SIZE;
+    bodies[i].z += bodies[i].dz * STEP_SIZE;
   }
 }
 
@@ -47,29 +69,29 @@ void save_results() {
 
   FILE* file = fopen(buf,"w");
   for (int i = 0; i < N; i++) {
-    fprintf(file, "%f,%f,%f,%f,%f,%f\n", x[i], y[i], z[i], dx[i], dy[i], dz[i]);
+    fprintf(file, "%f,%f,%f,%f,%f,%f\n", 
+      bodies[i].x,
+      bodies[i].y,
+      bodies[i].z,
+      bodies[i].dx,
+      bodies[i].dy,
+      bodies[i].dz);
   }
   fclose(file);
 }
 
 int main() {
-  m = aligned_alloc(64, N*sizeof(double));
-  x = aligned_alloc(64, N*sizeof(double));
-  y = aligned_alloc(64, N*sizeof(double));
-  z = aligned_alloc(64, N*sizeof(double));
-  dx = aligned_alloc(64, N*sizeof(double));
-  dy = aligned_alloc(64, N*sizeof(double));
-  dz = aligned_alloc(64, N*sizeof(double));
+  bodies = aligned_alloc(64, N*sizeof(struct Body));
 
-  m[0] = 6E24;
+  bodies[0].mass = 6E24;
   for (int i = 1; i<N; i++) {
-    m[i] = 10000 * (double)rand() / (double)(RAND_MAX);
-    dx[i] = 10*(((double)rand() / (double)(RAND_MAX))-0.5);
-    dy[i] = 60 + 10*(((double)rand() / (double)(RAND_MAX))-0.5);
-    dz[i] = 10*(((double)rand() / (double)(RAND_MAX))-0.5);
-    x[i] = -earth_radius;
-    y[i] = 0;
-    z[i] = 0;
+    bodies[i].mass = 10000 * (double)rand() / (double)(RAND_MAX);
+    bodies[i].dx = -300 + 100*(((double)rand() / (double)(RAND_MAX))-0.5);
+    bodies[i].dy = 6000 + 100*(((double)rand() / (double)(RAND_MAX))-0.5);
+    bodies[i].dz = 100*(((double)rand() / (double)(RAND_MAX))-0.5);
+    bodies[i].x = -earth_radius;
+    bodies[i].y = 0;
+    bodies[i].z = 0;
   }
 
   for (int i = 0; i <= ITERATIONS; i++) {
@@ -79,14 +101,10 @@ int main() {
     }
 
     // Update accelerations
-    update_velocity(x, dx, m);
-    update_velocity(y, dy, m);
-    update_velocity(z, dz, m);
+    update_velocity(bodies);
 
     // Update positions
-    update_position(x, dx);
-    update_position(y, dy);
-    update_position(z, dz);
+    update_position(bodies);
   }
 
   return 0;
