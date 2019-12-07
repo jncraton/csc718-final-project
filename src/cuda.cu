9cd1cb937f672215cca3ca274381b5fe0ee826da
Jon Craton <jncraton@gmail.com>
2019-12-07 00:01:21 -0500

    Check for null objects earlier

--- src/cuda.cu
+++ src/cuda.cu
@@ -7,37 +7,39 @@ __global__ void update_from_gravity (Body * bodies, int * N) {
 
   i = blockIdx.x + 1;
 
-  for (j = threadIdx.x; j < *N; j+=blockDim.x) {
-    if (i != j && bodies[i].mass && bodies[j].mass) {
-      // 12 Operations
-      r2 = (
-        (bodies[i].x-bodies[j].x) * (bodies[i].x-bodies[j].x) +
-        (bodies[i].y-bodies[j].y) * (bodies[i].y-bodies[j].y) +
-        (bodies[i].z-bodies[j].z) * (bodies[i].z-bodies[j].z)
-      );
-
-      double unit_gravity = rsqrt(r2) * STEP_SIZE * G * bodies[j].mass * (1/r2);
-
-      // 22 Operations (including 3 atomic)
-      if (r2 > (bodies[i].radius + bodies[j].radius) * (bodies[i].radius + bodies[j].radius)) {
-        atomicAdd(&bodies[i].dx, (bodies[j].x - bodies[i].x) * unit_gravity);
-        atomicAdd(&bodies[i].dy, (bodies[j].y - bodies[i].y) * unit_gravity);
-        atomicAdd(&bodies[i].dz, (bodies[j].z - bodies[i].z) * unit_gravity);
-      } else {
-          new_mass = bodies[i].mass + bodies[j].mass;
-          bodies[j].dx = (bodies[j].dx * bodies[j].mass + bodies[i].dx * bodies[i].mass) / new_mass;
-          bodies[j].dy = (bodies[j].dy * bodies[j].mass + bodies[i].dy * bodies[i].mass) / new_mass;
-          bodies[j].dz = (bodies[j].dz * bodies[j].mass + bodies[i].dz * bodies[i].mass) / new_mass;
-          bodies[j].mass = new_mass;
-
-          double volume = new_mass / earth_density;
-           // Volume = (4/3) pi r^3
-          // r^3 = volume * (3/4) / pi
-          double r3 = volume * (3.0/4.0) / 3.14159;
-
-          bodies[j].radius = cbrt(r3);
-          bodies[i].mass = 0.0;
-          bodies[i].radius = 0.0;
+  if (bodies[i].mass) {
+    for (j = threadIdx.x; j < *N; j+=blockDim.x) {
+      if (i != j && bodies[j].mass) {
+        // 12 Operations
+        r2 = (
+          (bodies[i].x-bodies[j].x) * (bodies[i].x-bodies[j].x) +
+          (bodies[i].y-bodies[j].y) * (bodies[i].y-bodies[j].y) +
+          (bodies[i].z-bodies[j].z) * (bodies[i].z-bodies[j].z)
+        );
+  
+        double unit_gravity = rsqrt(r2) * STEP_SIZE * G * bodies[j].mass * (1/r2);
+  
+        // 22 Operations (including 3 atomic)
+        if (r2 > (bodies[i].radius + bodies[j].radius) * (bodies[i].radius + bodies[j].radius)) {
+          atomicAdd(&bodies[i].dx, (bodies[j].x - bodies[i].x) * unit_gravity);
+          atomicAdd(&bodies[i].dy, (bodies[j].y - bodies[i].y) * unit_gravity);
+          atomicAdd(&bodies[i].dz, (bodies[j].z - bodies[i].z) * unit_gravity);
+        } else {
+            new_mass = bodies[i].mass + bodies[j].mass;
+            bodies[j].dx = (bodies[j].dx * bodies[j].mass + bodies[i].dx * bodies[i].mass) / new_mass;
+            bodies[j].dy = (bodies[j].dy * bodies[j].mass + bodies[i].dy * bodies[i].mass) / new_mass;
+            bodies[j].dz = (bodies[j].dz * bodies[j].mass + bodies[i].dz * bodies[i].mass) / new_mass;
+            bodies[j].mass = new_mass;
+  
+            double volume = new_mass / earth_density;
+             // Volume = (4/3) pi r^3
+            // r^3 = volume * (3/4) / pi
+            double r3 = volume * (3.0/4.0) / 3.14159;
+  
+            bodies[j].radius = cbrt(r3);
+            bodies[i].mass = 0.0;
+            bodies[i].radius = 0.0;
+        }
       }
     }
   }
