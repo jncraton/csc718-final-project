c2bace31218d82d175ae33955518524aa9da57c5
Jon Craton <jncraton@gmail.com>
2019-12-09 15:27:11 -0500

    Remove all explicit loops from CUDA code

--- src/cuda.cu
+++ src/cuda.cu
@@ -2,43 +2,39 @@
 
 __global__ void update_from_gravity (Body * bodies, int * N) {
   fptype r2, new_mass;
-  int i,j;
   fptype earth_density = earth_mass / ((4.0 / 3.0) * 3.14159 * earth_radius * earth_radius * earth_radius);
 
-  i = blockIdx.x + 1;
+  int i = blockIdx.x + 1;
+  int j = blockIdx.y * blockDim.y + threadIdx.x;
 
-  if (bodies[i].mass) {
-    for (j = threadIdx.x; j < *N; j+=blockDim.x) {
-      if (i != j && bodies[j].mass) {
-        r2 = (
-          (bodies[i].x-bodies[j].x) * (bodies[i].x-bodies[j].x) +
-          (bodies[i].y-bodies[j].y) * (bodies[i].y-bodies[j].y) +
-          (bodies[i].z-bodies[j].z) * (bodies[i].z-bodies[j].z)
-        );
-  
-        fptype unit_gravity = rsqrt(r2) * STEP_SIZE * G * bodies[j].mass * (1/r2);
-  
-        if (r2 > (bodies[i].radius + bodies[j].radius) * (bodies[i].radius + bodies[j].radius)) {
-          atomicAdd(&bodies[i].dx, (bodies[j].x - bodies[i].x) * unit_gravity);
-          atomicAdd(&bodies[i].dy, (bodies[j].y - bodies[i].y) * unit_gravity);
-          atomicAdd(&bodies[i].dz, (bodies[j].z - bodies[i].z) * unit_gravity);
-        } else {
-            new_mass = bodies[i].mass + bodies[j].mass;
-            bodies[j].dx = (bodies[j].dx * bodies[j].mass + bodies[i].dx * bodies[i].mass) / new_mass;
-            bodies[j].dy = (bodies[j].dy * bodies[j].mass + bodies[i].dy * bodies[i].mass) / new_mass;
-            bodies[j].dz = (bodies[j].dz * bodies[j].mass + bodies[i].dz * bodies[i].mass) / new_mass;
-            bodies[j].mass = new_mass;
-  
-            fptype volume = new_mass / earth_density;
-             // Volume = (4/3) pi r^3
-            // r^3 = volume * (3/4) / pi
-            fptype r3 = volume * (3.0/4.0) / 3.14159;
-  
-            bodies[j].radius = cbrt(r3);
-            bodies[i].mass = 0.0;
-            bodies[i].radius = 0.0;
-        }
-      }
+  if (i != j) {
+    r2 = (
+      (bodies[i].x-bodies[j].x) * (bodies[i].x-bodies[j].x) +
+      (bodies[i].y-bodies[j].y) * (bodies[i].y-bodies[j].y) +
+      (bodies[i].z-bodies[j].z) * (bodies[i].z-bodies[j].z)
+    );
+
+    fptype unit_gravity = rsqrt(r2) * STEP_SIZE * G * bodies[j].mass * (1/r2);
+
+    if (r2 > (bodies[i].radius + bodies[j].radius) * (bodies[i].radius + bodies[j].radius)) {
+      atomicAdd(&bodies[i].dx, (bodies[j].x - bodies[i].x) * unit_gravity);
+      atomicAdd(&bodies[i].dy, (bodies[j].y - bodies[i].y) * unit_gravity);
+      atomicAdd(&bodies[i].dz, (bodies[j].z - bodies[i].z) * unit_gravity);
+    } else {
+        new_mass = bodies[i].mass + bodies[j].mass;
+        bodies[j].dx = (bodies[j].dx * bodies[j].mass + bodies[i].dx * bodies[i].mass) / new_mass;
+        bodies[j].dy = (bodies[j].dy * bodies[j].mass + bodies[i].dy * bodies[i].mass) / new_mass;
+        bodies[j].dz = (bodies[j].dz * bodies[j].mass + bodies[i].dz * bodies[i].mass) / new_mass;
+        bodies[j].mass = new_mass;
+
+        fptype volume = new_mass / earth_density;
+         // Volume = (4/3) pi r^3
+        // r^3 = volume * (3/4) / pi
+        fptype r3 = volume * (3.0/4.0) / 3.14159;
+
+        bodies[j].radius = cbrt(r3);
+        bodies[i].mass = 0.0;
+        bodies[i].radius = 0.0;
     }
   }
 }
@@ -69,7 +65,7 @@ void update(struct Body * bodies, int iterations) {
   Body * cuda_bodies;
   int * cuda_N;
 
-  int threads_per_block = 32;
+  int threads_per_block = 1024;
 
   gpuErrchk(cudaMalloc(&cuda_bodies, (N + threads_per_block) * sizeof(Body)));
   gpuErrchk(cudaMemcpy(cuda_bodies, bodies, N * sizeof(Body), cudaMemcpyHostToDevice));
@@ -78,7 +74,8 @@ void update(struct Body * bodies, int iterations) {
   gpuErrchk(cudaMemcpy(cuda_N, &N, sizeof(int), cudaMemcpyHostToDevice));
 
   for (int i = 0; i < iterations; i++) {
-    update_from_gravity<<<N-1,threads_per_block>>>(cuda_bodies, cuda_N);
+    dim3 grid(N-1, N / threads_per_block);
+    update_from_gravity<<<grid,threads_per_block>>>(cuda_bodies, cuda_N);
 
     int blocks = 1 + N / threads_per_block;
     update_positions<<<blocks,threads_per_block>>>(cuda_bodies, cuda_N);
